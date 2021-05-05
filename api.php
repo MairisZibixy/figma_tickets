dc0212c067af8f5c8dee37d83843721c237afa8a
Mairis Karklins <zibitijs@gmail.com>
2021-05-05 21:42:48 +0300

    added files and changes for data to be on server

--- api.php
+++ api.php
@@ -1,3 +1,38 @@
 <?php
 
-echo file_get_contents('data.json');
+header('Content-type: application/json');
+
+include "DB.php";
+$db = new DB('localhost', 'root', 'root', 'contact_management');
+
+$db->fetchAll('tickets');
+
+$data = $db->getAll();
+
+if (is_array($data)) {
+    foreach ($data as &$row) {
+        if (array_key_exists('priority', $row)) {
+            switch ($row['priority']) {
+                case 1:
+                    $row['priority'] = 'low';
+                    break;
+                case 2:
+                    $row['priority'] = 'normal';
+                    break;
+                case 3:
+                    $row['priority'] = 'high';
+                    break;
+            }
+        }
+
+        if (array_key_exists('date_time', $row)) {
+            $datatime = new Datetime($row['date_time']);
+            $row['date'] = $datatime->format('M d, Y');
+            $row['time'] = $datatime->format('h:i A');
+            unset($row['date_time']);
+        }
+    }
+}
+
+
+echo json_encode(array_values($data));
---
+++ script.js
@@ -0,0 +1,11 @@
+function request(url, callback) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            callback(this.responseText);
+        }
+    };
+
+    xhttp.open("post", url);
+    xhttp.send();
+}
